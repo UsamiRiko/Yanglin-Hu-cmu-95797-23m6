27328aa9da40b76ec710025350cbad258e1c76df
Yanglin Hu <yanglinh@andrew.cmu.edu>
2023-07-10 09:33:36 +0800

    move to another directory, init dbt, get sources

---
+++ nyc_transit_dbt/models/staging/stg__central_park_weather.sql
@@ -0,0 +1,23 @@
+with source as (
+
+	select * from {{ source('NYC-transit', 'central_park_weather') }}
+	
+),
+
+renamed as (
+
+	SELECT
+          STATION,
+          NAME,
+          CAST(DATE AS DATE) AS DATE,
+          CAST(AWND AS DOUBLE) AS AWND,
+          CAST(PRCP AS DOUBLE) AS PRCP,
+          CAST(SNOW AS DOUBLE) AS SNOW,
+          CAST(SNWD AS DOUBLE) AS SNWD,
+          CAST(TMAX AS INT) AS TMAX,
+          CAST(TMIN AS INT) AS TMIN,
+          filename	
+        FROM source	
+)
+
+select distinct * from renamed
